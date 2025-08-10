ef59633a469c576af128626d4236e59f3f410bca
wtc33 <wtc3300@gmail.com>
2025-08-10 21:34:27 +0800

    二次修改

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,13 @@ import Navigation from './router';
 export default function Home() {
 
   return (
-    <Box style={{padding:'0',margin:'0',display:'flex',width:'100vw',height:'100vh',overflow:'hidden'}}>
+    <div style={{padding:'0',margin:'0',display:'flex',width:'100vw',height:'100vh',overflow:'hidden'}}>
       <Navigation />
       <div className="rt">
+        <div className="notic" style={{position:"absolute",bottom:0,width:'100%',textAlign:'center',color:'#7a7979'}}>
+          <p>E-mail:wtc3300@gmail.com &nbsp;&nbsp; This website is mainly used for technical learning, all resources are from the Internet, if there is any infringement, please contact the email.</p>
+        </div>
       </div>
-    </Box>
+    </div>
   );
 }
--- src/app/router.tsx
+++ src/app/router.tsx
@@ -7,7 +7,7 @@ function Navigation() {
   const router = useRouter();
   return (
     <div className="lt">
-      <h5 style={{textAlign:"center",marginTop:"10px"}}>小破站</h5>
+      <h5 style={{textAlign:"center",marginTop:"10px",overflow:"hidden"}}>小破站</h5>
       <Button style={{width:"100%",marginTop:"10px"}} variant="contained" onClick={()=> router.push('/')}>首页</Button>
       <Button style={{width:"100%",marginTop:"10px"}} variant="contained" onClick={()=> router.push('./Link')}>链接集合</Button>
       <Button style={{width:"100%",marginTop:"10px"}} variant="contained" onClick={()=> router.push('./Game')}>游戏集合</Button>
